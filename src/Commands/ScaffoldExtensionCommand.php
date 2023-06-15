906c82faff14b608901e14243146d4b08ad1cd29
Chris Reynolds <chris@jazzsequence.com>
2023-06-15 13:03:07 -0600

    psr-2 cs fixes

--- src/Commands/ScaffoldExtensionCommand.php
+++ src/Commands/ScaffoldExtensionCommand.php
@@ -9,88 +9,92 @@ namespace Pantheon\TerminusScaffoldExtension\Commands;
 
 use Pantheon\Terminus\Commands\TerminusCommand;
 
-class ScaffoldExtensionCommand extends TerminusCommand {
+class ScaffoldExtensionCommand extends TerminusCommand
+{
 
-	/**
-	 * Run the scaffold_extension UJR job.
-	 *
-	 * @command scaffold-extension
-	 * @aliases se
-	 * @aliases scaffold
-	 */
-	public function scaffoldExtension() {
-		$this->log()->notice("Attempting to run scaffold_extension job...");
+    /**
+     * Run the scaffold_extension UJR job.
+     *
+     * @command scaffold-extension
+     * @aliases se
+     * @aliases scaffold
+     */
+    public function scaffoldExtension()
+    {
+        $this->log()->notice("Attempting to run scaffold_extension job...");
+    }
 
-	}
+    /**
+     * Run the scaffold_extension UJR job.
+     *
+     * @command scaffold-extension:run
+     * @aliases se:run
+     * @aliases scaffold:run
+     *
+     * @param string $site_env
+     * @param string $job_id
+     */
+    public function runExtension(string $site_env, $job_id = '')
+    {
+        $site = $this->decypherSiteInfo($site_env);
+        $site_id = $site['id'];
+        $env = $site['env'];
 
-	/**
-	 * Run the scaffold_extension UJR job.
-	 *
-	 * @command scaffold-extension:run
-	 * @aliases se:run
-	 * @aliases scaffold:run
-	 *
-	 * @param string $site_env
-	 * @param string $job_id
-	 */
-	public function runExtension( string $site_env, $job_id = '' ) {
-		$site = $this->decypherSiteInfo( $site_env );
-		$site_id = $site['id'];
-		$env = $site['env'];
+        if (empty($job_id)) {
+            $this->log()->error('Please provide a job ID.');
+            return;
+        }
 
-		if ( empty( $job_id ) ) {
-			$this->log()->error( 'Please provide a job ID.' );
-			return;
-		}
+        $this->log()->notice(sprintf('Attempting to run the %1$s job on %2$s.%3$s...', $job_id, $site_id, $env));
+    }
 
-		$this->log()->notice(sprintf( 'Attempting to run the %1$s job on %2$s.%3$s...', $job_id, $site_id, $env ));
-	}
+    /**
+     * List the available scaffold_extension UJR jobs.
+     *
+     * @command scaffold-extension:list
+     * @aliases se:list
+     * @aliases scaffold:list
+     *
+     * @param string $site_env
+     */
+    public function listJobs(string $site_env)
+    {
+        $site = $this->decypherSiteInfo($site_env);
+        $site_id = $site['id'];
+        $env = $site['env'];
 
-	/**
-	 * List the available scaffold_extension UJR jobs.
-	 *
-	 * @command scaffold-extension:list
-	 * @aliases se:list
-	 * @aliases scaffold:list
-	 *
-	 * @param string $site_env
-	 */
-	public function listJobs( string $site_env ) {
-		$site = $this->decypherSiteInfo( $site_env );
-		$site_id = $site['id'];
-		$env = $site['env'];
+        $this->log()->notice(sprintf('Attempting to list jobs on %1$s.%2$s...', $site_id, $env));
+    }
 
-		$this->log()->notice(sprintf( 'Attempting to list jobs on %1$s.%2$s...', $site_id, $env ));
-	}
+    /**
+     * Given a $site_env which might be in site_id.dev or just site_id format, return an array with the site_id and env.
+     *
+     * The default environment is dev.
+     *
+     * @param string $site_env
+     *
+     * @return array
+     */
+    private function decypherSiteInfo(string $site_env) : array
+    {
+        $env = 'dev';
+        $site_id = $site_env;
 
-	/**
-	 * Given a $site_env which might be in site_id.dev or just site_id format, return an array with the site_id and env.
-	 *
-	 * The default environment is dev.
-	 *
-	 * @param string $site_env
-	 *
-	 * @return array
-	 */
-	private function decypherSiteInfo( string $site_env ) : array {
-		$env = 'dev';
-		$site_id = $site_env;
+        /**
+         * If the first parameter has a period (.) in it, then it is a $site_id.$env combination.
+         * Otherwise, we default to dev.
+         */
+        if (strpos($site_env, '.') !== false) {
+            $site_props = explode('.', $site_env);
+            $site_id = $site_props[0];
+            $env = $site_props[1];
+        }
 
-		/**
-		 * If the first parameter has a period (.) in it, then it is a $site_id.$env combination.
-		 * Otherwise, we default to dev.
-		 */
-		if ( strpos( $site_env, '.' ) !== false ) {
-			$site_props = explode( '.', $site_env );
-			$site_id = $site_props[0];
-			$env = $site_props[1];
-		}
+        $site = [
+            'id' => $site_id,
+            'env' => $env
+        ];
 
-		$site = [
-			'id' => $site_id,
-			'env' => $env
-		];
-
-		return $site;
-	}
+        return $site;
+    }
 }
