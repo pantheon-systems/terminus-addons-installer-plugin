c42d08288281d5d315de9417c873ded4fd8f0de9
Chris Reynolds <chris@jazzsequence.com>
2023-07-21 14:33:55 -0600

    make sure the user associated with the machine token can access the site

--- .bin/set-up-globals.sh
+++ .bin/set-up-globals.sh
@@ -4,7 +4,16 @@ set -ex
 TERMINUS_PLUGINS_DIR=.. terminus list -n remote
 
 set +ex
-echo "Test site is $TERMINUS_SITE"
 echo "Logging in with a machine token:"
 terminus auth:login -n --machine-token="$TERMINUS_TOKEN"
 terminus whoami
+
+# Check if TERMINUS_SITE is not empty.
+if [ -z "$TERMINUS_SITE" ]; then
+  echo "TERMINUS_SITE is not defined. Please set it to the name of your Pantheon site if needed for this step."
+else
+  echo "Test site is $TERMINUS_SITE"
+  # Make sure the current user has access to the site.
+  terminus site:info "$TERMINUS_SITE"
+  terminus wp "$TERMINUS_SITE.dev" -- core version
+fi
