09b6e2be8592009846d8a41f56f7c432b5fa2be5
Chris Reynolds <chris@jazzsequence.com>
2023-07-24 15:13:58 -0600

    properly define the environment

--- .bin/set-up-globals.sh
+++ .bin/set-up-globals.sh
@@ -2,9 +2,10 @@
 set -ex
 
 TERMINUS_PLUGINS_DIR=.. terminus list -n remote
+SITE_ENV="${TERMINUS_SITE}.ci-${BUILD_NUM}"
 
 set +ex
 echo "Logging in with a machine token:"
 terminus auth:login -n --machine-token="$TERMINUS_TOKEN"
 terminus whoami
-terminus multidev:create --no-db --no-files $TERMINUS_SITE ci-$BUILD_NUM
+terminus multidev:create --no-db --no-files $SITE_ENV
