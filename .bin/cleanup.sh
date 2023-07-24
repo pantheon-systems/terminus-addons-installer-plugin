ed9d8b8bcee316b7c4a262a2598ff8a2715b9507
Chris Reynolds <chris@jazzsequence.com>
2023-07-24 15:23:25 -0600

    remove $

--- .bin/cleanup.sh
+++ .bin/cleanup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set +ex
-$SITE_ENV="${TERMINUS_SITE}.ci-${BUILD_NUM}"
+SITE_ENV="${TERMINUS_SITE}.ci-${BUILD_NUM}"
 # TODO: Remove the plugin after it is installed via the workflow.
 terminus multidev:delete --delete-branch --yes $SITE_ENV
