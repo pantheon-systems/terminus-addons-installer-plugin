ad77d5acf1b060397220a0ad6ca443d868bc25e0
Chris Reynolds <chris@jazzsequence.com>
2023-07-24 15:19:10 -0600

    properly set the source environment

--- .bin/cleanup.sh
+++ .bin/cleanup.sh
@@ -2,4 +2,4 @@
 set +ex
 $SITE_ENV = "${TERMINUS_SITE}.ci-${BUILD_NUM}"
 # TODO: Remove the plugin after it is installed via the workflow.
-terminus multidev:delete --delete-branch $SITE_ENV
+terminus multidev:delete --delete-branch --yes $SITE_ENV
--- .bin/set-up-globals.sh
+++ .bin/set-up-globals.sh
@@ -2,10 +2,9 @@
 set -ex
 
 TERMINUS_PLUGINS_DIR=.. terminus list -n remote
-SITE_ENV="${TERMINUS_SITE}.ci-${BUILD_NUM}"
 
 set +ex
 echo "Logging in with a machine token:"
 terminus auth:login -n --machine-token="$TERMINUS_TOKEN"
 terminus whoami
-terminus multidev:create --no-db --no-files $SITE_ENV
+terminus multidev:create --no-db --no-files $TERMINUS_SITE.dev ci-$BUILD_NUM
