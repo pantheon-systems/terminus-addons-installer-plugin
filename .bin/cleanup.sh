bcddef2b42e594e239313561695843a8b256b387
Chris Reynolds <chris@jazzsequence.com>
2023-07-24 15:09:29 -0600

    create a new multidev for each run

--- .bin/cleanup.sh
+++ .bin/cleanup.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 set +ex
+$SITE_ENV = "${TERMINUS_SITE}.ci-${BUILD_NUM}"
 # TODO: Remove the plugin after it is installed via the workflow.
+terminus multidev:delete --delete-branch $SITE_ENV
--- .bin/set-up-globals.sh
+++ .bin/set-up-globals.sh
@@ -7,3 +7,4 @@ set +ex
 echo "Logging in with a machine token:"
 terminus auth:login -n --machine-token="$TERMINUS_TOKEN"
 terminus whoami
+terminus multidev:create --no-db --no-files $TERMINUS_SITE ci-$BUILD_NUM
