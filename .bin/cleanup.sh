1f16d816d6943f7f635742cacda2e155e4f7dec6
Chris Reynolds <chris@jazzsequence.com>
2023-07-26 09:51:21 -0600

    add a cleanup step to switch back to git mode and abandon any uncommitted changes we made during testing

--- .bin/cleanup.sh
+++ .bin/cleanup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set +ex
 SITE_ENV="${TERMINUS_SITE}.ci-${BUILD_NUM}"
-# TODO: Remove the plugin after it is installed via the workflow.
+# Switch back to Git mode after functional tests switched to SFTP.
+terminus connection:set $SITE_ENV git --yes
+# Delete the multidev environment.
 terminus multidev:delete --delete-branch --yes $SITE_ENV
