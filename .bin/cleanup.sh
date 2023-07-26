542a5c49f2459558cbc70c73d5e6e208ffae2e84
Chris Reynolds <chris@jazzsequence.com>
2023-07-26 10:18:18 -0600

    if we're deleting the multidev, there's no reason to switch to git

--- .bin/cleanup.sh
+++ .bin/cleanup.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 set +ex
 SITE_ENV="${TERMINUS_SITE}.ci-${BUILD_NUM}"
-# Switch back to Git mode after functional tests switched to SFTP.
-terminus connection:set $SITE_ENV git --yes
 # Delete the multidev environment.
 terminus multidev:delete --delete-branch --yes $SITE_ENV
