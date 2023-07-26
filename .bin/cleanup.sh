7551e7ed518b92c688217810255f34090529a527
Chris Reynolds <chris@jazzsequence.com>
2023-07-26 13:57:11 -0600

    fix cleanup script

--- .bin/cleanup.sh
+++ .bin/cleanup.sh
@@ -4,10 +4,10 @@ SITE_ENV="${TERMINUS_SITE}.ci-${BUILD_NUM}"
 # Nuke any lingering multidev environments from orbit.
 for ENV in $(terminus multidev:list --field=Name --format=list $TERMINUS_SITE); do
   if [[ $ENV == ci-* ]]; then
-	terminus multidev:delete --delete-branch --yes $ENV
+	terminus multidev:delete --delete-branch --yes ${TERMINUS_SITE}.${ENV}
   fi
   # Also dump any localtests environments.
   if [[ $ENV == localtests ]]; then
-	terminus multidev:delete --delete-branch --yes $ENV
+	terminus multidev:delete --delete-branch --yes ${TERMINUS_SITE}.localtests
   fi
 done
