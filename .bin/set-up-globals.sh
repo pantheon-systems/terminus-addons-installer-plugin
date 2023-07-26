db67705b166ee5f61681b019b7a001c40ecb2761
Chris Reynolds <chris@jazzsequence.com>
2023-07-26 14:24:00 -0600

    create the multidev with a db copy

--- .bin/set-up-globals.sh
+++ .bin/set-up-globals.sh
@@ -7,7 +7,7 @@ set +ex
 echo "Logging in with a machine token:"
 terminus auth:login -n --machine-token="$TERMINUS_TOKEN"
 terminus whoami
-terminus multidev:create --no-db --no-files $TERMINUS_SITE.dev ci-$BUILD_NUM
+terminus multidev:create $TERMINUS_SITE.dev ci-$BUILD_NUM
 terminus connection:set $TERMINUS_SITE.ci-$BUILD_NUM git
 # Check if ~/.ssh directory exists
 if [ ! -d ~/.ssh ]; then
