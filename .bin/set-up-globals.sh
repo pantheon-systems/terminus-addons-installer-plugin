57aafb140324dbe421899cd1ba3257c7e16102f9
Chris Reynolds <chris@jazzsequence.com>
2023-07-27 13:25:23 -0600

    more linting fixes

--- .bin/set-up-globals.sh
+++ .bin/set-up-globals.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-set -ex
+set -e
 
 TERMINUS_PLUGINS_DIR=.. terminus list -n remote
 
-set +ex
 echo "Logging in with a machine token:"
 terminus auth:login -n --machine-token="$TERMINUS_TOKEN"
 terminus whoami
-terminus multidev:create $TERMINUS_SITE.dev ci-$BUILD_NUM
-terminus connection:set $TERMINUS_SITE.ci-$BUILD_NUM git
+terminus multidev:create "$TERMINUS_SITE".dev ci-"$BUILD_NUM"
+terminus connection:set "$TERMINUS_SITE".ci-"$BUILD_NUM" git
 # Check if ~/.ssh directory exists
 if [ ! -d ~/.ssh ]; then
 	mkdir ~/.ssh
@@ -21,10 +20,10 @@ if [ ! -f ~/.ssh/config ]; then
 	chmod 600 ~/.ssh/config
 fi
 
-echo "Editing the ~/.ssh/config file"
-# Set StrictHostKeyChecking to no in ~/.ssh/config
-echo "StrictHostKeyChecking no" >> ~/.ssh/config
-# Set LogLevel to ERROR in ~/.ssh/config
-echo "LogLevel ERROR" >> ~/.ssh/config
-# Set UserKnownHostsFile to /dev/null in ~/.ssh/config
-echo "UserKnownHostsFile /dev/null" >> ~/.ssh/config
+{
+	echo "Editing the ~/.ssh/config file"
+	echo "Host *"
+	echo "  StrictHostKeyChecking no"
+	echo "  LogLevel ERROR"
+	echo "  UserKnownHostsFile /dev/null"
+} >> ~/.ssh/config
