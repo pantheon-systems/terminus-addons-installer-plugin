d85d30950310035825b6f87f6424e6fd750cb2cb
Chris Reynolds <chris@jazzsequence.com>
2023-07-27 13:29:26 -0600

    also fix in the cleanup

--- .bin/cleanup.sh
+++ .bin/cleanup.sh
@@ -3,7 +3,7 @@ set -ex
 
 # Nuke any lingering multidev environments from orbit.
 for ENV in $(terminus multidev:list --field=Name --format=list "$TERMINUS_SITE"); do
-  if [[ "$ENV" == ci-"$BUILD_NUM" || "$ENV" == localtests || "$ENV" == failure-test ]]; then
+  if [[ "$ENV" == ci-"$BUILD_NUM" || "$ENV" == localtests || "$ENV" == fs-test-"$BUILD_NUM" ]]; then
 	terminus multidev:delete --delete-branch --yes "${TERMINUS_SITE}"."${ENV}"
   fi
 done
