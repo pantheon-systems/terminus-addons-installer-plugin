b37eb9cd8d919da89c1963de94b84564a8e650f4
Chris Reynolds <chris@jazzsequence.com>
2023-07-27 13:21:22 -0600

    fix linting and also delete failure test multidevs

--- .bin/cleanup.sh
+++ .bin/cleanup.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-set +ex
-SITE_ENV="${TERMINUS_SITE}.ci-${BUILD_NUM}"
+set -ex
+
 # Nuke any lingering multidev environments from orbit.
-for ENV in $(terminus multidev:list --field=Name --format=list $TERMINUS_SITE); do
-  if [[ $ENV == ci-* ]]; then
-	terminus multidev:delete --delete-branch --yes ${TERMINUS_SITE}.${ENV}
-  fi
-  # Also dump any localtests environments.
-  if [[ $ENV == localtests ]]; then
-	terminus multidev:delete --delete-branch --yes ${TERMINUS_SITE}.localtests
+for ENV in $(terminus multidev:list --field=Name --format=list "$TERMINUS_SITE"); do
+  if [[ "$ENV" == ci-* || "$ENV" == localtests || "$ENV" == failure-test ]]; then
+	terminus multidev:delete --delete-branch --yes "${TERMINUS_SITE}"."${ENV}"
   fi
 done
