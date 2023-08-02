9213e43fcdb5383a9ef2f37d93f8ec49b3fa6962
Chris Reynolds <chris@jazzsequence.com>
2023-08-02 09:33:27 -0600

    move test prep script to its own file

---
+++ .bin/set-up-globals.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -e
+
+# Check if TERMINUS_SITE is defined, otherwise define it as terminus-addons-installer-plugin.
+if [ -z "$TERMINUS_SITE" ]; then
+  # Looks like this is a local run. We'll create the localtests multidev ro test this.
+  TERMINUS_SITE=terminus-addons-installer-plugin
+
+  # Check if the localtests environment exists already, otherwise create it. This can be passed locally as an environment variable but defaults to localtests.
+
+  if [ -z "$LOCALENV" ]; then
+    LOCALENV=localtests
+  fi
+
+  output=$(terminus multidev:list "$TERMINUS_SITE" || true)
+  if ! echo "$output" | grep -q "${LOCALENV}"; then
+    terminus multidev:create $TERMINUS_SITE.dev ${LOCALENV}
+  fi
+  SITE_ENV="$TERMINUS_SITE"."$LOCALENV"
+  FS_TEST_ENV="$TERMINUS_SITE".fs-test
+
+  # If we're in a local run, let's create and set up the multidev early. On CI runs, we do this in set-up-globals.sh.
+  # Create a new multidev just for this failure test
+  terminus multidev:create "$TERMINUS_SITE".dev fs-test
+
+  # Switch to SFTP mode.
+  terminus connection:set "$TERMINUS_SITE".fs-test sftp
+
+  # Install the Hello Dolly plugin.
+  terminus wp "$TERMINUS_SITE".fs-test -- plugin install hello-dolly
+else
+  # Always use the multidev if in CI.
+  SITE_ENV="$TERMINUS_SITE".ci-"$BUILD_NUM"
+  FS_TEST_ENV="$TERMINUS_SITE".fs-test-"$BUILD_NUM"
+fi
--- tests/functional/0-confirm-install.bats
+++ tests/functional/0-confirm-install.bats
@@ -1,5 +1,7 @@
 #!/usr/bin/env bats
 
+source ./bin/set-up-globals.sh
+
 #
 # confirm-install.bats
 #
@@ -25,5 +27,17 @@
 @test "get help on addons-install:run command" {
   run terminus help addons-install:run
   [[ $output == *"Run the specified job"* ]]
+  [ "$status" -eq 0 ]
+}
+
+@test "make sure multidevs were created successfully" {
+  multidev_list=$(terminus multidev:list "$TERMINUS_SITE" || true)
+
+  run echo "$multidev_list" | grep -q "${FS_TEST_ENV}"
+  [[ $output == *"Created"* ]]
+  [ "$status" -eq 0 ]
+
+  run echo "$multidev_list" | grep -q "${SITE_ENV}"
+  [[ $output == *"Created"* ]]
   [ "$status" -eq 0 ]
 }
