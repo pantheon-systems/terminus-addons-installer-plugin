63fc8fbbbbfe04f03d7261e062cf69844eb68539
Chris Reynolds <chris@jazzsequence.com>
2023-08-03 11:44:58 -0600

    add debugging for source path

--- tests/bin/run-tests.sh
+++ tests/bin/run-tests.sh
@@ -3,5 +3,14 @@ if [ -n "$GITHUB_WORKSPACE" ]; then
     echo "Running in GitHub workflow. Setting up globals."
     source_path="$GITHUB_WORKSPACE/$source_path"
 fi
-source $source_path
+echo "Source path: $source_path"
+
+if [ -f "$source_path" ]; then
+    echo "Sourcing $source_path"
+    source "$source_path"
+else
+    echo "ERROR: File not found at $source_path"
+    exit 1
+fi
+
 TERMINUS_PLUGINS_DIR=.. PATH=tools/bin:$PATH bats -p -t tests/functional
