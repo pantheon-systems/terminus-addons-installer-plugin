27a5fdb85667042e3b5d331f0ba32a70872b29f5
Chris Reynolds <chris@jazzsequence.com>
2023-08-03 11:30:48 -0600

    fix the github workspace path

--- tests/bin/run-tests.sh
+++ tests/bin/run-tests.sh
@@ -1,7 +1,7 @@
 source_path=tests/bin/set-up-globals.sh
-if [ -n "$GITHUB_WORKFLOW" ]; then
-	echo "Running in GitHub workflow. Setting up globals."
-	source_path="$GITHUB_WORKFLOW"/tests/bin/set-up-globals.sh
+if [ -n "$GITHUB_WORKSPACE" ]; then
+    echo "Running in GitHub workflow. Setting up globals."
+    source_path="$GITHUB_WORKSPACE/$source_path"
 fi
 source $source_path
 TERMINUS_PLUGINS_DIR=.. PATH=tools/bin:$PATH bats -p -t tests/functional
