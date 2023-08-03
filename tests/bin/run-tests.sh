f6faa57fc776c54033c9fc253e4da014ac783f15
Chris Reynolds <chris@jazzsequence.com>
2023-08-03 12:08:00 -0600

    remove debugging info and exit on errors

--- tests/bin/run-tests.sh
+++ tests/bin/run-tests.sh
@@ -1,16 +1,10 @@
+set -e
 source_path=tests/bin/set-up-globals.sh
 if [ -n "$GITHUB_WORKSPACE" ]; then
     echo "Running in GitHub workflow. Setting up globals."
     source_path="$GITHUB_WORKSPACE/$source_path"
 fi
-echo "Source path: $source_path"
 
-if [ -f "$source_path" ]; then
-    echo "Sourcing $source_path"
-    . "$source_path"
-else
-    echo "ERROR: File not found at $source_path"
-    exit 1
-fi
+. "$source_path"
 
 TERMINUS_PLUGINS_DIR=.. PATH=tools/bin:$PATH bats -p -t tests/functional
