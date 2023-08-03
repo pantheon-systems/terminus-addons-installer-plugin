e3874510aef710e79cc002194999dacd4dce50b0
Chris Reynolds <chris@jazzsequence.com>
2023-08-03 11:20:15 -0600

    update the source path if we're running in GHA

--- tests/bin/run-tests.sh
+++ tests/bin/run-tests.sh
@@ -1,2 +1,7 @@
-source tests/bin/set-up-globals.sh
+source_path=tests/bin/set-up-globals.sh
+if [ -n "$GITHUB_WORKFLOW" ]; then
+	echo "Running in GitHub workflow. Setting up globals."
+	source_path="$GITHUB_WORKFLOW"/tests/bin/set-up-globals.sh
+fi
+source $source_path
 TERMINUS_PLUGINS_DIR=.. PATH=tools/bin:$PATH bats -p -t tests/functional
