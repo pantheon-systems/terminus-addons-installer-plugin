8cb40d3d5fbb8c079529c3c1186c102c276516f7
Chris Reynolds <chris@jazzsequence.com>
2023-08-03 10:48:54 -0600

    handle fs-test environments when the builds are > 1000

--- .bin/setup.sh
+++ .bin/setup.sh
@@ -3,14 +3,21 @@ set -e
 
 TERMINUS_PLUGINS_DIR=.. terminus list -n remote
 
+# Set the fs-test number. If the build number is > 999, we need to trim the -test- out of the middle.
+if [ "$BUILD_NUM" -gt 999 ]; then
+	FS_TEST="fs-${BUILD_NUM}"
+else
+	FS_TEST="fs-test-${BUILD_NUM}"
+fi
+
 echo "Logging in with a machine token:"
 terminus auth:login -n --machine-token="$TERMINUS_TOKEN"
 terminus whoami
 terminus multidev:create "$TERMINUS_SITE".dev ci-"$BUILD_NUM"
 terminus connection:set "$TERMINUS_SITE".ci-"$BUILD_NUM" git
 # Set up the environment for filesystem tests.
-terminus multidev:create "$TERMINUS_SITE".dev fs-test-"$BUILD_NUM"
-terminus connection:set "$TERMINUS_SITE".fs-test-"$BUILD_NUM" sftp
+terminus multidev:create "$TERMINUS_SITE".dev "$FS_TEST"
+terminus connection:set "$TERMINUS_SITE"."$FS_TEST" sftp
 
 # Check if ~/.ssh directory exists
 if [ ! -d ~/.ssh ]; then
@@ -32,4 +39,4 @@ fi
 	echo "  UserKnownHostsFile /dev/null"
 } >> ~/.ssh/config
 
-terminus wp "$TERMINUS_SITE".fs-test-"$BUILD_NUM" -- plugin install hello-dolly
+terminus wp "$TERMINUS_SITE"."$FS_TEST" -- plugin install hello-dolly
