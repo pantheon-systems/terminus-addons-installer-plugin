67d3dcce407b62ae1532a1e5e3b4183e68bf7b93
Chris Reynolds <chris@jazzsequence.com>
2023-10-25 13:37:17 -0600

    fuzzy match with the PHP VERSION

--- .bin/cleanup.sh
+++ .bin/cleanup.sh
@@ -3,7 +3,7 @@ set -ex
 
 # Nuke any lingering multidev environments from orbit.
 for ENV in $(terminus multidev:list --field=Name --format=list "$TERMINUS_SITE"); do
-  if [[ "$ENV" == ci-* || "$ENV" == localtests || "$ENV" == fs-* ]]; then
+  if [[ "$ENV" =~ ^ci-.*-"$PHP_VERSION" || "$ENV" == localtests || "$ENV" =~ ^fs-.*-"$PHP_VERSION" ]]; then
     echo "Deleting $ENV."
 	  terminus multidev:delete --delete-branch --yes "${TERMINUS_SITE}"."${ENV}"
   fi
