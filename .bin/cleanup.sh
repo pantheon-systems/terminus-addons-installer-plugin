b7704b086a4fd20b2f9b025f80626b7f2c241e8e
Chris Reynolds <chris@jazzsequence.com>
2023-10-25 13:13:40 -0600

    delete any un-deleted ci- environment

--- .bin/cleanup.sh
+++ .bin/cleanup.sh
@@ -3,7 +3,7 @@ set -ex
 
 # Nuke any lingering multidev environments from orbit.
 for ENV in $(terminus multidev:list --field=Name --format=list "$TERMINUS_SITE"); do
-  if [[ "$ENV" == ci-"$BUILD_NUM"-"$PHP_VERSION" || "$ENV" == localtests || "$ENV" == fs-* ]]; then
+  if [[ "$ENV" == ci-* || "$ENV" == localtests || "$ENV" == fs-* ]]; then
 	terminus multidev:delete --delete-branch --yes "${TERMINUS_SITE}"."${ENV}"
   fi
 done
