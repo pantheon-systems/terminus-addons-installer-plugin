61008f1eec440c95dfe5ac8538fd51b6473df047
Chris Reynolds <chris@jazzsequence.com>
2023-10-25 14:22:33 -0600

    strip the period from the php version for cleanup

--- .bin/cleanup.sh
+++ .bin/cleanup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -ex
-
+PHP_VERSION=$(echo "$PHP_VERSION" | tr -d '.')
 # Nuke any lingering multidev environments from orbit.
 for ENV in $(terminus multidev:list --field=Name --format=list "$TERMINUS_SITE"); do
   if [[ "$ENV" =~ ^ci-.*-"$PHP_VERSION" || "$ENV" == localtests || "$ENV" =~ ^fs-.*-"$PHP_VERSION" ]]; then
